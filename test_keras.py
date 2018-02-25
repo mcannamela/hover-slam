d76fa59704194b43011b46e191f02639dfaae616
Michael Cannamela <m.cannamela@gmail.com>
2018-02-25 14:51:13 -0500

    fancier test keras

--- test_keras.py
+++ test_keras.py
@@ -1,13 +1,25 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import regularizers, optimizers
 import numpy as np
+from bokeh import plotting as plt
+import os
 
 model = Sequential()
-model.add(Dense(units=32, activation='relu', input_dim=1))
-model.add(Dense(units=16, activation='relu'))
+reg = 1e-5
+model.add(Dense(units=32, activation='relu', input_dim=1,
+                kernel_regularizer=regularizers.l2(reg),
+                activity_regularizer=regularizers.l2(reg)
+                ))
+model.add(Dense(units=32, activation='relu', input_dim=1,
+                kernel_regularizer=regularizers.l2(reg),
+                activity_regularizer=regularizers.l2(reg)
+                ))
 model.add(Dense(units=10, activation='softmax'))
+
+opt = optimizers.SGD(lr=0.2, decay=1e-3, momentum=1e-5, nesterov=True)
 model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer=opt,
               metrics=['accuracy'])
 
 
@@ -22,8 +34,27 @@ def build_data(n_samples):
 x_train, y_train = build_data(10000)
 x_test, y_test = build_data(10000)
 
+bound = np.arange(9) * .1 + .1
+delta_minus = (bound - x_train[:, None])
+delta_plus = (x_train[:, None] - bound)
+delta_plus[delta_plus < 0] = 1.0
+delta_minus[delta_minus < 0] = 1.0
+gaps = np.min(delta_plus, axis=0) + np.min(delta_minus, axis=0)
+
+
 # x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
-model.fit(x_train, y_train, epochs=40, batch_size=32)
+model.fit(x_train, y_train, epochs=60, batch_size=32)
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-classes = model.predict(x_test, batch_size=128)
 print(loss_and_metrics)
+print (gaps, 1.0-np.sum(gaps))
+
+x_mesh = np.linspace(0, 1, 100000)
+classes = np.argmax(model.predict(x_mesh, batch_size=128), axis=1)
+x = np.concatenate([[0], x_mesh[np.flatnonzero(np.diff(classes) > 0)], [1]])
+print(np.diff(x))
+
+f = plt.figure()
+f.segment(x, x * 0, x, x * 0 + 1)
+f.circle(x, x * 0 + 1)
+plt.output_file(os.path.join("resources", "bokeh_tmp", "test_keras.html"))
+plt.show(f)
